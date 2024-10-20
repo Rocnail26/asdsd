1a40e791607c86930b26fa60953bd64c65083469
jbohiques <jrojas@bohiques.com>
2024-10-20 15:27:58 -0400

    get resident fix overfetching

--- src/services/Residence/getResidence.ts
+++ src/services/Residence/getResidence.ts
@@ -9,11 +9,32 @@ try {
             ...data
         },
         include: {
-            Community:true,
-            Expense:true,
-            Owner:true,
-            ResidenceType: true,
-            Resident: true
+            Owner: {
+                omit:{
+                    role_id:true,
+                    password:true,
+                    community_id:true,
+                    isActive:true,
+                    isVerified:true,
+                    residence_id:true,
+                    createdAt:true
+                }
+            },
+            ResidenceType: {
+                select:{
+                     title:true,
+                     description:true
+                }
+            },
+            Resident: {
+                select:{
+                    id:true,
+                    firstName:true,
+                    lastName:true,
+                    phone:true,
+                    email:true
+                }
+            }
         },
         omit:{
             owner_id:true,
